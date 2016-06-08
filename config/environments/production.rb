7d975fea4cd4372c43088938701b17424e3ac68a
sathibabu-nyros <sathibabu_nyros@yahoo.com>
2016-06-08 14:09:49 +0530

    update production for mailer heroku support

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,8 +84,8 @@ Rails.application.configure do
     address:              'smtp.gmail.com',
     port:                  587,
     domain:               'example.com',
-    user_name:            'sathipatchipala@gmail.com',
-    password:             'f93swEsw',
+    user_name:            '--',
+    password:             '--',
     authentication:       'plain',
     enable_starttls_auto: true  }
 end
