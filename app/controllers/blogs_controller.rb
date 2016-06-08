8ff99d0f9ccc9e51ac09fd2b43217b64ac7b86ed
sathibabu-nyros <sathibabu_nyros@yahoo.com>
2016-06-08 11:39:37 +0530

    add mailer

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -80,12 +80,12 @@ class BlogsController < ApplicationController
   def share
     @blog = Blog.find(params[:blog_id])
     if @blog.present? 
-      
+
        reciepents = current_user.friendships.where(:status => true).map {|friendship| friendship.friend.email}    
        reciepents << current_user.inverse_friendships.where(:status => true).map {|friend| friend.user.email}  
    
       reciepents.flatten.each do |user|        
-         # BlogMailer.share_blog(@blog,user).deliver_now 
+         BlogMailer.share_blog(@blog,user).deliver_now 
       end 
 
       respond_to do |format|
